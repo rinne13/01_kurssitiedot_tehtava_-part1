3a34a0d7a496a82f9925332b616c7ccf0b9b87aa
rinne13 <varvararinne@gmail.com>
2024-12-04 11:47:46 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/02-part2-react-kurssitiedot", 
+  base: "01_kurssitiedot_tehtava_-part1", 
   plugins: [react()],
 })
